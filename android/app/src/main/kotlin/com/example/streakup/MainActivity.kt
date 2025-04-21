420e6586f2a2f44638e89d632e594089dc8b5b14
Mohammad Soqar <mnsoqar1@gmail.com>
2025-04-21 10:31:50 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/streakup/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.company.streakup
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:streakup/ui/themes/theme.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(); // Ensure Firebase is initialized
+  runApp(
+    MultiProvider(
+      providers: [
+        /* ChangeNotifierProvider(
+            create: (_) => AuthViewModel()), */
+        // Add your providers here
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: const MaterialTheme(TextTheme()).light(),
+      darkTheme: const MaterialTheme(TextTheme()).dark(),
+      themeMode: ThemeMode.system,
+      home: const AuthWrapper(),
+    );
+  }
+}
+
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+              body: Center(child: CircularProgressIndicator()));
+        } else {
+          if (snapshot.hasData) {
+            return Container(); // Show Home Screen
+          }
+          return Container(); // Show Splash Screen
+        }
+      },
+    );
+  }
+}
