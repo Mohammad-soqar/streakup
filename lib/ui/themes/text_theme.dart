1253cdf5505d26f54fcc0d1c0363c6fa5df56dc9
Mohammad Soqar <mnsoqar1@gmail.com>
2025-04-27 23:28:21 +0300

    adding material theme library

---
+++ lib/ui/themes/text_theme.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+final TextTheme customTextTheme = TextTheme(
+  displayLarge: TextStyle( // Display Large
+    fontFamily: 'Roboto',
+    fontSize: 44,
+    height: 52 / 44,
+    fontWeight: FontWeight.bold,
+    letterSpacing: -0.25,
+  ),
+  displayMedium: TextStyle( // Display Medium
+    fontFamily: 'Roboto',
+    fontSize: 36,
+    height: 44 / 36,
+    fontWeight: FontWeight.bold,
+    letterSpacing: -0.15,
+  ),
+  displaySmall: TextStyle( // Display Small
+    fontFamily: 'Roboto',
+    fontSize: 32,
+    height: 40 / 32,
+    fontWeight: FontWeight.bold,
+    letterSpacing: -0.1,
+  ),
+  headlineLarge: TextStyle( // Headline Large
+    fontFamily: 'Roboto',
+    fontSize: 28,
+    height: 36 / 28,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 0.0,
+  ),
+  headlineMedium: TextStyle( // Headline Medium
+    fontFamily: 'Roboto',
+    fontSize: 26,
+    height: 32 / 26,
+    fontWeight: FontWeight.w600, // SemiBold
+    letterSpacing: 0.0,
+  ),
+  headlineSmall: TextStyle( // Headline Small
+    fontFamily: 'Roboto',
+    fontSize: 24,
+    height: 32 / 24,
+    fontWeight: FontWeight.w600, // SemiBold
+    letterSpacing: 0.05,
+  ),
+  titleLarge: TextStyle( // Title Large
+    fontFamily: 'Roboto',
+    fontSize: 22,
+    height: 28 / 22,
+    fontWeight: FontWeight.w400, // Regular
+    letterSpacing: 0.1,
+  ),
+  titleMedium: TextStyle( // Title Medium
+    fontFamily: 'Roboto',
+    fontSize: 20,
+    height: 28 / 20,
+    fontWeight: FontWeight.w400, // Regular
+    letterSpacing: 0.15,
+  ),
+  titleSmall: TextStyle( // Title Small
+    fontFamily: 'Roboto',
+    fontSize: 18,
+    height: 24 / 18,
+    fontWeight: FontWeight.w400, // Regular
+    letterSpacing: 0.1,
+  ),
+  labelLarge: TextStyle( // Label Large
+    fontFamily: 'Roboto',
+    fontSize: 16,
+    height: 24 / 16,
+    fontWeight: FontWeight.w400, // Regular
+    letterSpacing: 0.15,
+  ),
+  labelMedium: TextStyle( // Label Medium
+    fontFamily: 'Roboto',
+    fontSize: 14,
+    height: 20 / 14,
+    fontWeight: FontWeight.w600, // SemiBold
+    letterSpacing: 0.2,
+  ),
+  labelSmall: TextStyle( // Label Small
+    fontFamily: 'Roboto',
+    fontSize: 12,
+    height: 16 / 12,
+    fontWeight: FontWeight.w600, // SemiBold
+    letterSpacing: 0.25,
+  ),
+  bodyLarge: TextStyle( // Body Large
+    fontFamily: 'Roboto',
+    fontSize: 16,
+    height: 24 / 16,
+    fontWeight: FontWeight.w400, // Regular
+    letterSpacing: 0.2,
+  ),
+  bodyMedium: TextStyle( // Body Medium
+    fontFamily: 'Roboto',
+    fontSize: 14,
+    height: 20 / 14,
+    fontWeight: FontWeight.w400, // Regular
+    letterSpacing: 0.25,
+  ),
+  bodySmall: TextStyle( // Body Small
+    fontFamily: 'Roboto',
+    fontSize: 12,
+    height: 16 / 12,
+    fontWeight: FontWeight.w400, // Regular
+    letterSpacing: 0.3,
+  ),
+);
